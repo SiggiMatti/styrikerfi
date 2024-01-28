8a2570cfb00789490b522c6ab61514024e2c54ec
Arnar Bjarki <47283718+arnarbb@users.noreply.github.com>
2024-01-28 17:05:48 +0000

    Update run_program.c
    better comments

--- verk2/p2/run_program.c
+++ verk2/p2/run_program.c
@@ -8,40 +8,41 @@
 #define ERROR_CODE 255
 
 int run_program(char *file_path, char *args[]) {
-    // Check if file_path is NULL
+    // Validates if the file_path is not NULL
     if (file_path == NULL) {
         return ERROR_CODE;
     }
 
     pid_t pid = fork();
     
-    // Error in fork
+    // Checks for fork error, returns ERROR_CODE if fork fails
     if (pid < 0) {
         return ERROR_CODE;
     }
 
-    // Child process
+    // Executes in the child process
     if (pid == 0) {
-        // Execute the program
+        // Attempts to execute the program specified by file_path
         if (execvp(file_path, args) == -1) {
-            // In case of an error in execvp, exit with ERROR_CODE
+            // If execvp fails, the child process exits with ERROR_CODE
             exit(ERROR_CODE);
         }
     }
 
-    // Parent process
+    // Executes in the parent process
     int status;
+    // Waits for the child process to terminate and checks for errors in waitpid
     if (waitpid(pid, &status, 0) == -1) {
-        // Error in waitpid
         return ERROR_CODE;
     }
 
+    // Checks if the child process terminated normally
     if (WIFEXITED(status)) {
         int exit_status = WEXITSTATUS(status);
-        // Return the exit status of the child process
+        // Returns the exit status of the child process
         return exit_status;
     } else {
-        // If the child process did not terminate normally, return ERROR_CODE
+        // Returns ERROR_CODE if the child process did not terminate normally
         return ERROR_CODE;
     }
 }
