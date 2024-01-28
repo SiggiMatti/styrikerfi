98dac612becf91484d561a0e8606df57ba48bd1b
Arnar Bjarki <47283718+arnarbb@users.noreply.github.com>
2024-01-28 16:52:04 +0000

    Update run_program.c

--- verk2/p2/run_program.c
+++ verk2/p2/run_program.c
@@ -1,12 +1,47 @@
 #include "run_program.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define ERROR_CODE 255
 
-int run_program(char *file_path, char *argv[])
-{
-    (void) file_path;
-    (void) argv;
+int run_program(char *file_path, char *args[]) {
+    // Check if file_path is NULL
+    if (file_path == NULL) {
+        return ERROR_CODE;
+    }
 
-    // remember to return ERROR_CODE on error.
-    return ERROR_CODE;
+    pid_t pid = fork();
+    
+    // Error in fork
+    if (pid < 0) {
+        return ERROR_CODE;
+    }
+
+    // Child process
+    if (pid == 0) {
+        // Execute the program
+        if (execvp(file_path, args) == -1) {
+            // In case of an error in execvp, exit with ERROR_CODE
+            exit(ERROR_CODE);
+        }
+    }
+
+    // Parent process
+    int status;
+    if (waitpid(pid, &status, 0) == -1) {
+        // Error in waitpid
+        return ERROR_CODE;
+    }
+
+    if (WIFEXITED(status)) {
+        int exit_status = WEXITSTATUS(status);
+        // Return the exit status of the child process
+        return exit_status;
+    } else {
+        // If the child process did not terminate normally, return ERROR_CODE
+        return ERROR_CODE;
+    }
 }
